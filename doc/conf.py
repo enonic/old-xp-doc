acb7746463fe8f66da0fb19dbdfaedfffc57f21a
striky <tlo@enonic.com>
2015-08-21 18:32:14 +0200

    Testing custom css

--- doc/conf.py
+++ doc/conf.py
@@ -101,35 +101,6 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
-
-
-# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    # Override default css to get a larger width for local build
-    def setup(app):
-        #app.add_javascript("custom.js")
-        app.add_stylesheet('css/enonic.css')
-else:
-    # Override default css to get a larger width for ReadTheDoc build
-    html_context = {
-        'css_files': [
-            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-            '_static/css/enonic.css',
-        ],
-    }
-
-
-
-
-
-
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
