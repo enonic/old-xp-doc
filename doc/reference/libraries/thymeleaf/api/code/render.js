4dede3ab8929ef05c379be4ce7c5b4599cd5d3c8
Sten Roger Sandvik <srs@enonic.com>
2015-09-24 21:52:44 +0200

    Fixed docs for view render functions

--- doc/reference/libraries/thymeleaf/api/code/render.js
+++ doc/reference/libraries/thymeleaf/api/code/render.js
@@ -1,18 +1,16 @@
 var view = resolve('view/fruit.html');
+var model = {
+  fruits: [
+    {
+        name: 'Apple',
+        color: 'Red'
+    },
+    {
+        name: 'Pear',
+        color: 'Green'
+    }
+  ]
+};
 
 var thymeleaf = require('/lib/xp/thymeleaf');
-var result = thymeleaf.render({
-    view: view,
-    model: {
-        fruits: [
-            {
-                name: 'Apple',
-                color: 'Red'
-            },
-            {
-                name: 'Pear',
-                color: 'Green'
-            }
-        ]
-    }
-});
+var result = thymeleaf.render(view, model);
--- doc/reference/libraries/xslt/api/code/render.js
+++ doc/reference/libraries/xslt/api/code/render.js
@@ -1,18 +1,16 @@
 var view = resolve('view/fruit.xsl');
+var model = {
+  fruits: [
+    {
+        name: 'Apple',
+        color: 'Red'
+    },
+    {
+        name: 'Pear',
+        color: 'Green'
+    }
+  ]
+};
 
 var xslt = require('/lib/xp/xslt');
-var result = xslt.render({
-    view: view,
-    model: {
-        fruits: [
-            {
-                name: 'Apple',
-                color: 'Red'
-            },
-            {
-                name: 'Pear',
-                color: 'Green'
-            }
-        ]
-    }
-});
+var result = xslt.render(view, model);
