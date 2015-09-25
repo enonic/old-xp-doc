fac47084885bcd8ee70136b2a01a8797a746b8cd
Glenn Ricaud <gri@enonic.com>
2015-09-25 15:45:37 +0200

    XP-1637 Documentation - Fix error in query documentation (both in 6.0 and newer)

--- doc/reference/libraries/content/api/code/query.js
+++ doc/reference/libraries/content/api/code/query.js
@@ -1,75 +1,74 @@
 var contentLib = require('/lib/xp/content');
 
 var result = contentLib.query({
-            "start": 0,
-            "count": 3,
-            "sort": "_modifiedTime DESC, geodistance('p1', 'p2')",
-            "query": "type = 'article' AND fulltext('myField', 'searching for cheese', 'AND') ",
-            "contentTypes": [
-                app.name + ":article",
-                app.name + ":comment"
-            ],
+    "start": 0,
+    "count": 2,
+    "sort": "_modifiedTime DESC, geoDistance('data.location', '59.91,10.75')",
+    "query": "data.city = 'Oslo' AND fulltext('data.description', 'garden', 'AND') ",
+    "contentTypes": [
+        app.name + ":house",
+        app.name + ":apartment"
+    ],
+    "aggregations": {
+        "floors": {
+            "terms": {
+                "field": "data.number_floor",
+                "order": "_count asc"
+            },
             "aggregations": {
-                "genders": {
-                    "terms": {
-                        "field": "gender",
-                        "order": "_count asc",
-                        "size": 2
-                    },
-                    "aggregations": {
-                        "prices": {
-                            "histogram": {
-                                "field": "price",
-                                "interval": 50,
-                                "extendedBoundMin": 0,
-                                "extendedBoundMax": 500,
-                                "minDocCount": 0,
-                                "order": "_key desc"
-                            }
-                        }
-                    }
-                },
-                "by_month": {
-                    "dateHistogram": {
-                        "field": "init_date",
-                        "interval": "1M",
+                "prices": {
+                    "histogram": {
+                        "field": "data.price",
+                        "interval": 1000000,
+                        "extendedBoundMin": 1000000,
+                        "extendedBoundMax": 3000000,
                         "minDocCount": 0,
-                        "format": "MM-yyy"
-                    }
-                },
-                "price_ranges": {
-                    "range": {
-                        "field": "price",
-                        "ranges": [
-                            {"to": 50},
-                            {"from": 50, "to": 100},
-                            {"from": 100}
-                        ]
-                    }
-                },
-                "my_date_range": {
-                    "dateRange": {
-                        "field": "date",
-                        "format": "MM-yyy",
-                        "ranges": [
-                            {"to": "now-10M/M"},
-                            {"from": "now-10M/M"}
-                        ]
-                    }
-                },
-                "time_stats": {
-                    "stats": {
-                        "field": "item_count"
+                        "order": "_key desc"
                     }
                 }
             }
-        });
+        },
+        "by_month": {
+            "dateHistogram": {
+                "field": "data.publish_date",
+                "interval": "1M",
+                "minDocCount": 0,
+                "format": "MM-yyyy"
+            }
+        },
+        "price_ranges": {
+            "range": {
+                "field": "data.price",
+                "ranges": [
+                    {"to": 2000000},
+                    {"from": 2000000, "to": 3000000},
+                    {"from": 3000000}
+                ]
+            }
+        },
+        "my_date_range": {
+            "dateRange": {
+                "field": "data.publish_date",
+                "format": "MM-yyyy",
+                "ranges": [
+                    {"to": "now-10M/M"},
+                    {"from": "now-10M/M"}
+                ]
+            }
+        },
+        "price_stats": {
+            "stats": {
+                "field": "data.price"
+            }
+        }
+    }
+});
 
 log.info('Found ' + result.total + ' number of contents');
 
-for (var i = 0; i < result.contents.length; i++) {
-    var content = result.contents[i];
-    log.info('Content ' + content._name + ' loaded');
+for (var i = 0; i < result.hits.length; i++) {
+    var content = result.hits[i];
+    log.info('Content ' + content._name + ' found');
 }
 
 
