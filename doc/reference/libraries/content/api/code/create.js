02339a28ca3f4d0f8073b32fe1addaad3f4e1ae6
Glenn Ricaud <gri@enonic.com>
2015-10-05 13:29:07 +0200

    XP-1745 Documentation - Fix error in create documentation (both in 6.0 and newer)

--- doc/reference/libraries/content/api/code/create.js
+++ doc/reference/libraries/content/api/code/create.js
@@ -1,30 +1,45 @@
 var contentLib = require('/lib/xp/content');
 
 var result = contentLib.create({
-        name: 'mycontent',
-        parentPath: '/a/b',
-        displayName: 'My Content',
-        draft: true,
-        contentType: 'test:myContentType',
-        language: 'no',
-        data: {
-            a: 1,
-            b: 2,
-            c: ['1', '2'],
-            d: {
-                e: {
-                    f: 3.6,
-                    g: true
-                }
-            }
-        },
-        x: {
-            "com-enonic-myapplication": {
-                myschema: {
-                    a: 1
-                }
+    name: 'mycontent',
+    parentPath: '/features/js-libraries',
+    displayName: 'My Content',
+    requireValid: true,
+    contentType: app.name + ':all-input-types',
+    branch: 'draft',
+    language: 'no',
+    data: {
+        myCheckbox: true,
+        myComboBox: 'option1',
+        myDate: '1970-01-01',
+        myDateTime: '1970-01-01T10:00',
+        myDouble: 3.14,
+        myGeoPoint: '59.91,10.75',
+        myHtmlArea: '<p>htmlAreaContent</p>',
+        myImageSelector: '5a5fc786-a4e6-4a4d-a21a-19ac6fd4784b',
+        myLong: 123,
+        myRelationship: 'features',
+        myRadioButtons: 'option1',
+        myTag: 'aTag',
+        myTextArea: 'textAreaContent',
+        myTextLine: 'textLineContent',
+        myTime: '10:00',
+        myTextAreas: [
+            'textAreaContent1',
+            'textAreaContent2'
+        ],
+        myItemSet: {
+            'textLine': 'textLineContent',
+            'long': 123
+        }
+    },
+    x: {
+        "com-enonic-app-features": {
+            "menu-item": {
+                "menuItem": true
             }
         }
-    });
+    }
+});
 
 log.info('Content created with id ' + result._id);
