64ee8ef84f052fe1e7460160703a5f56521a6d69
Alex Rodriguez <aro@enonic.com>
2015-10-09 09:55:10 +0200

    XP-1718 Page Preview: map should not be shown, when value for location not saved

--- doc/developer/my-first-app/code/city-part/city-list.js
+++ doc/developer/my-first-app/code/city-part/city-list.js
@@ -3,44 +3,56 @@ var portal = require('/lib/xp/portal'); // Import the portal functions
 var thymeleaf = require('/lib/xp/thymeleaf'); // Import the Thymeleaf rendering function
 
 // Handle the GET request
-exports.get = function(req) {
+exports.get = function (req) {
 
     // Get the part configuration for the map
     var config = portal.getComponent().config;
-    var zoom = parseInt(config.zoom) && config.zoom <= 15 && config.zoom >= 1? config.zoom : 10;
+    var zoom = parseInt(config.zoom) && config.zoom <= 15 && config.zoom >= 1 ? config.zoom : 10;
     var mapType = config.mapType || 'ROADMAP';
 
     // String that will be inserted to the head of the document
     var googleMaps = '<script src="http://maps.googleapis.com/maps/api/js"></script>';
 
-    // Get the country content and extract the needed data from the JSON
-    var result = contentLib.getChildren({
-        key: portal.getContent()._id
+    var countryPath = portal.getContent()._path;
+
+    // Get all the country's cities
+    var result = contentLib.query({
+        start: 0,
+        count: 100,
+        contentTypes: [
+            app.name + ':city'
+        ],
+        "query": "_path LIKE '/content" + countryPath + "/*'",
     });
+
     var hits = result.hits;
 
     var cities = [];
 
-    if(hits.length > 0) {
+    if (hits.length > 0) {
         googleMaps += '<script>function initialize() {';
 
         // Loop through the contents and extract the needed data
-        for(var i = 0; i < hits.length; i++) {
+        for (var i = 0; i < hits.length; i++) {
 
             var city = {};
             city.name = hits[i].displayName;
             city.location = hits[i].data.location;
-            city.population = hits[i].data.population? 'Population: ' + hits[i].data.population : null;
-            city.mapId = 'googleMap' + i;
+            city.population = hits[i].data.population ? 'Population: ' + hits[i].data.population : null;
 
             cities.push(city);
 
-            googleMaps += 'var center' + i + ' = new google.maps.LatLng(' + city.location + '); '
+            if (city.location) {
+                city.mapId = 'googleMap' + i;
 
-            googleMaps += 'var mapProp = {center:center' + i + ', zoom:' + zoom +
-                ', mapTypeId:google.maps.MapTypeId.' + mapType + ', scrollwheel: false };' +
-                'var map' + i + ' = new google.maps.Map(document.getElementById("googleMap' + i + '"),mapProp); ' +
-                'var marker = new google.maps.Marker({ position:center' + i + '}); marker.setMap(map' + i + ');';
+                googleMaps += 'var center' + i + ' = new google.maps.LatLng(' + city.location + '); ';
+
+                googleMaps += 'var mapProp = {center:center' + i + ', zoom:' + zoom +
+                              ', mapTypeId:google.maps.MapTypeId.' + mapType + ', scrollwheel: false };' +
+                              'var map' + i + ' = new google.maps.Map(document.getElementById("googleMap' + i + '"),mapProp); ' +
+                              'var marker = new google.maps.Marker({ position:center' + i + '}); marker.setMap(map' + i + ');';
+            }
+
         }
 
         googleMaps += '} google.maps.event.addDomListener(window, "load", initialize);</script>';
