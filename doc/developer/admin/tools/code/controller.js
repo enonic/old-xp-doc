5f1337576b69192c2b5d7e767cc8bfb985829e98
Glenn Ricaud <gri@enonic.com>
2016-11-28 10:03:31 +0100

    XP-4521 Adding the Launcher Panel menu - Assets not found

--- doc/developer/admin/tools/code/controller.js
+++ doc/developer/admin/tools/code/controller.js
@@ -1,13 +1,15 @@
 var mustache = require('/lib/xp/mustache');
 var portalLib = require('/lib/xp/portal');
+var timestamp = Date.now();
 
 function handleGet() {
     var view = resolve('./my-view.html');
 
     var params = {
         adminUrl: portalLib.url({path: "/admin"}),
-        assetsUri: portalLib.url({path: "/admin/assets"}),
-        appId: 'my-custom-tool'
+        assetsUri: portalLib.url({path: "/admin/assets/" + timestamp}),
+        appId: 'my-custom-tool',
+        appName: 'My custom tool'
     };
     return {
         contentType: 'text/html',
