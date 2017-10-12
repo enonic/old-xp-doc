548ef518a5cc2d17918af35ef96d88a94532aa8d
Bobby <bellfalasch@gmail.com>
2017-10-12 12:53:04 +0200

    Simplify example to lessen information overload

--- doc/developer/ssjs/code/cookies.js
+++ doc/developer/ssjs/code/cookies.js
@@ -1,12 +1,9 @@
 return {
     status: 200,
     body: "Hello World",
-    headers: {
-        "header1": "value1"
-    },
     cookies: {
-        "plain": "value",
-        "complex": {
+        "plain": "value", // Example (1) of just setting a value
+        "complex": { // Example (2) for using a full JS object
             value: "value",
             path: "/valid/path",
             domain: "enonic.com",
