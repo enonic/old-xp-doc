66f686cff4bbb7d12a46e0868e3d76f44bd5ae7d
Bobby <bellfalasch@gmail.com>
2017-11-10 09:24:41 +0100

    Add config-example

--- doc/developer/ssjs/code/app.js
+++ doc/developer/ssjs/code/app.js
@@ -3,3 +3,6 @@ var name = app.name;  // com.enonic.app.superhero
 
 // Get application version
 var version = app.version;  // 1.2.0
+
+// Get some config from the <app.name>.cfg file
+var myKey = app.config.secretkey; // Reads the string stored in the "secretkey" property
